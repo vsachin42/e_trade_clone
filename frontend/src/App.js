a1601df4cce1331a4cc8f02eac6499a14beaecc2
Shreekant-totla <shreekanttotla3@gmail.com>
2023-07-21 21:58:18 +0530

    navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
+// import Navbar from './components/xyz';
+import { BrowserRouter } from 'react-router-dom';
+import { LandingPage } from './Pages/LandingPage';
 
 function App() {
   return (
     <ChakraProvider>
-      <Navbar />
+      <BrowserRouter>
+      {/* <Navbar /> */}
       {/* Your other components and content will go here */}
+      <LandingPage/>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
