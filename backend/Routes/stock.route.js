4a52681982badd1cf77e15d0c51621d8cbdae395
Sachin <soniji7269@gmail.com>
2023-07-24 08:06:02 +0530

    BE

--- backend/Routes/stock.route.js
+++ backend/Routes/stock.route.js
@@ -5,12 +5,51 @@ const { stockModel } = require("../Model/stock.model");
 const stockRouter = express.Router();
 
 
-stockRouter.get("/", async(req, res) => {
-    try{
+stockRouter.get('/', async (req, res) => {
+    try {
+      const page = parseInt(req.query.page); 
+      const limit = parseInt(req.query.limit);
+  
+      if(page && limit){
+        const skip = (page - 1) * limit;
+  
+      const items = await stockModel.find().skip(skip).limit(limit);
+  
+      const totalItems = await stockModel.countDocuments();
+  
+      if (items.length === 0) {
+        return res.status(404).json({ message: 'Page not found' });
+      }
+  
+      res.status(200).json({
+        page,
+        limit,
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit),
+        data: items,
+      });
+      }else{
         const stocks = await stockModel.find();
-        res.status(200).json(stocks);
+        res.status(200).json(stocks)
+      }
+
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
+
+stockRouter.get("/:id", async(req,res) => {
+    try{
+     const id = req.params.id;
+     const stock = await stockModel.findOne({_id:id});
+     if(stock){
+        res.status(200).json({stock});
+     }else{
+        res.status(404).json({msg: "Stock not found"});
+     }
     }catch(err){
-        res.status(400).json({error:err});
+        res.status(404).json({error: err});
     }
 })
 
@@ -56,8 +95,6 @@ stockRouter.delete("/delete/:id", async(req,res) => {
         res.status(400).json({error});
     }
 })
-
-
 
 
 
