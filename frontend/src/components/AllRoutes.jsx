75b13848abcc162c8948445a1e89d242d57cc711
Rinkesh375 <rinkeshujjwal16@gmail.com>
2023-07-24 14:29:29 +0530

    project done

--- frontend/src/components/AllRoutes.jsx
+++ frontend/src/components/AllRoutes.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import IndexSensex from './IndexSensex';
+import SingleProduct from '../Pages/SingleProduct';
 // import LogIn from '../Pages/logInPage';
 import OpenAccount from '../Pages/signUpPage';
 import { LandingPage } from '../Pages/LandingPage';
 import LogIn from '../Pages/logInPage';
+import AddToCart from '../Pages/AddToCart';
+
 
 const AllRoutes = () => {
   return (
        <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path="/stocks" element={<IndexSensex/>} />
-        <Route path='/stock/:id'/>
+        <Route path='/stock/:id' element={<SingleProduct/>}/>
+        <Route path='/addcart' element={<AddToCart/>} />
        </Routes> 
   );
 }
